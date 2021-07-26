c468e3e96dcea14c2bc6a99afe927fb83f04d58a
Alexander Milevski <alexander@linkurio.us>
2021-07-26 21:48:07 +0200

    Added dotenv

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -4,6 +4,8 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+import {} from '@env';
+
 export default function Login() {
   return (
     <View style={styles.container}>
---
+++ types/env.d.ts
@@ -0,0 +1,7 @@
+declare module '@env' {
+  export const API_BASE: string;
+  export const GOOGLE_ANDROID_ID: string;
+  export const GOOGLE_IOS_ID: string;
+  export const GOOGLE_WEB_ID: string;
+  export const FACEBOOK_APP_ID: string;
+}
