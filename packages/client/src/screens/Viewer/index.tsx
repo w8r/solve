8c182b053b4de6852f7d0c566c8402727a286c30
Alexander Milevski <alexander@linkurio.us>
2022-02-06 16:03:58 +0100

    Figuaring out Canvas files

--- packages/client/src/screens/Viewer/index.tsx
+++ packages/client/src/screens/Viewer/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { ViewerProps } from '../../navigation/types';
+import Canvas from '../../components/Canvas';
 
 export default function Viewer({ route }: ViewerProps) {
   console.log(route);
   return (
     <View>
       <Text>Viewer</Text>
+      <Canvas />
     </View>
   );
 }
