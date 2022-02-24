65cb6bb628160a44ca10be754d6e22dd6325c055
Yiğit Akbulut <yigitakbulut@icloud.com>
2022-02-25 00:15:13 +0100

    Fix VirtualizedList error

--- packages/client/src/screens/Dashboard/Placeholder.tsx
+++ packages/client/src/screens/Dashboard/Placeholder.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import { Box, Center, FlatList, ScrollView } from 'native-base';
+import { Box, Center, FlatList } from 'native-base';
 
 export default function Placeholder() {
   return (
-    <ScrollView>
-      <Box padding="10" flex={1} p="5">
-        <Center>
-          <FlatList
-            numColumns={5}
-            data={Array(10)
-              .fill(0)
-              .map(() => ({}))}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({ index }) => (
-              <Box
-                bg="blueGray.200"
-                p="2"
-                rounded="sm"
-                height="40"
-                minWidth="40"
-                marginRight="10"
-                marginBottom="10"
-              />
-            )}
-          />
-        </Center>
-      </Box>
-    </ScrollView>
+        <Box padding="10" flex={1} p="5">
+          <Center>
+            <FlatList
+            ListHeaderComponent={() => null}   
+            ListFooterComponent={() => null}   
+              numColumns={5}
+              data={Array(10)
+                .fill(0)
+                .map(() => ({}))}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ index }) => (
+                <Box
+                  bg="blueGray.200"
+                  p="2"
+                  rounded="sm"
+                  height="40"
+                  minWidth="40"
+                  marginRight="10"
+                  marginBottom="10"
+                />
+              )}
+            />
+          </Center>
+        </Box>
   );
 }
