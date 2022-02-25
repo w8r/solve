3b7dfd467190da861b03316ee573be5cb7c21a03
Yiğit Akbulut <yigitakbulut@icloud.com>
2022-02-25 19:02:17 +0100

    Improved node creation dialog

--- packages/client/src/components/CreateNodeDialog.tsx
+++ packages/client/src/components/CreateNodeDialog.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Modal } from 'native-base';
+import React, { SetStateAction, useRef, useState } from 'react';
+import { LogBox, StyleSheet, View } from 'react-native';
+import { Modal, Slider } from 'native-base';
 import { useFormik as useForm } from 'formik';
 import { FormContainer } from './FormContainer';
 import * as Validator from 'yup';
+import TagGroup from './TagGroup';
 
 import {
   Button,
@@ -16,93 +17,155 @@ import {
   Input,
   Divider
 } from 'native-base';
+import { BottomDrawer, DrawerState } from './BottomDrawer';
 
-const NodeDialogSchema = Validator.object().shape({
-  password: Validator.string()
-    .min(6, 'Too short')
-    .max(16, 'Too long')
-    .required('Required'),
-  email: Validator.string().email('Invalid email').required('Required')
-});
+const possibleSizes = [3, 3.5, 4, 5, 6, 7, 8, 9, 10, 12, 16, 20, 24];
+
+const getClosestSize = (goal: number) => {
+  return possibleSizes.reduce(function (prev: number, curr: number) {
+    return Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev;
+  });
+};
+
+const NodeDialogSchema = Validator.object().shape({});
+
+export interface Tag {
+  id: number;
+  label: string;
+}
+
+LogBox.ignoreLogs(['Cannot update a']);
 
 export default function CreateNodeDialog({
   visibility,
-  setVisibility
+  setVisibility,
+  setSelected
 }: {
   visibility: boolean;
   setVisibility: (visibility: boolean) => void;
+  setSelected: (selected: string) => void;
 }) {
   const { handleChange, handleBlur, submitForm, values, errors, touched } =
     useForm({
       validationSchema: NodeDialogSchema,
-      initialValues: { email: '', password: '' },
-      onSubmit: (values) => {}
+      initialValues: { selectedTags: [], name: '' },
+      onSubmit: (values) => {
+        console.log(values);
+      }
     });
 
-  const nodeNameRef = useRef<any>(null);
-  return (
-    <Modal
-      isOpen={visibility}
-      style={styles.modal}
-      isKeyboardDismissable={true}
-      animationPreset="slide"
-      avoidKeyboard={true}
-      closeOnOverlayClick={true}
-    >
-      <View style={styles.bottomNavigationView}>
-        <Text style={styles.closeButton} onPress={() => setVisibility(false)}>
-          Close
-        </Text>
+  const updateTags = (tags: string[]): void => {
+    handleChange('selectedTags')(tags.join(','));
+  };
+  const [onChangeValue, setOnChangeValue] = React.useState(10);
 
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'space-between'
-          }}
-        >
-          <FormContainer>
-            <Heading size="md" fontWeight="400" color="coolGray.800">
-              Add a new node
-            </Heading>
-            <VStack space={3} mt="5">
-              <FormControl>
-                <FormControl.Label
-                  _text={{
-                    color: 'coolGray.800',
-                    fontSize: 'xs',
-                    fontWeight: 500
-                  }}
+  const nodeNameRef = useRef<any>(null);
+  const tagRef = useRef<string[]>(null);
+  let tags = [];
+  if (visibility) {
+    return (
+      <BottomDrawer
+        initialState={DrawerState.Peek + 100}
+        onDrawerStateChange={(e) => null}
+      >
+        <View style={styles.bottomNavigationView}>
+          <Text style={styles.closeButton} onPress={() => setVisibility(false)}>
+            Close
+          </Text>
+
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'column',
+              justifyContent: 'space-between'
+            }}
+          >
+            <FormContainer>
+              <Heading size="md" fontWeight="400" color="coolGray.800">
+                Add a new node
+              </Heading>
+              <VStack space={4} mt="7">
+                <FormControl>
+                  <FormControl.Label
+                    _text={{
+                      color: 'coolGray.800',
+                      fontSize: 'xs',
+                      fontWeight: 500
+                    }}
+                  >
+                    Name
+                  </FormControl.Label>
+                  <Input
+                    onChange={(evt: any) =>
+                      handleChange('name')(evt.nativeEvent.text)
+                    }
+                    onBlur={handleBlur('name')}
+                    onSubmitEditing={() => nodeNameRef.current?.focus()}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormControl.Label
+                    _text={{
+                      color: 'coolGray.800',
+                      fontSize: 'xs',
+                      fontWeight: 500
+                    }}
+                  >
+                    Categories
+                  </FormControl.Label>
+                  <TagGroup
+                    source={['One', 'Two', 'Three']}
+                    onSelectedTagChange={updateTags}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormControl.Label
+                    _text={{
+                      color: 'coolGray.800',
+                      fontSize: 'xs',
+                      fontWeight: 500
+                    }}
+                  >
+                    Node Size
+                  </FormControl.Label>
+                  <Slider
+                    defaultValue={onChangeValue}
+                    onChange={(size) =>
+                      setOnChangeValue(getClosestSize(Math.floor(size / 5)))
+                    }
+                    colorScheme="orange"
+                  >
+                    <Slider.Track>
+                      <Slider.FilledTrack />
+                    </Slider.Track>
+                    <Slider.Thumb size={onChangeValue} />
+                  </Slider>
+                </FormControl>
+                <Button
+                  mt="2"
+                  colorScheme="indigo"
+                  _text={{ color: 'white' }}
+                  onPress={() => submitForm()}
                 >
-                  Name
-                </FormControl.Label>
-                <Input
-                  onBlur={handleBlur('name')}
-                  onSubmitEditing={() => nodeNameRef.current?.focus()}
-                />
-              </FormControl>
-              <Button
-                mt="2"
-                colorScheme="indigo"
-                _text={{ color: 'white' }}
-                onPress={() => submitForm()}
-              >
-                Add
-              </Button>
-            </VStack>
-            <Divider my="2" />
-          </FormContainer>
+                  Add
+                </Button>
+              </VStack>
+              <Divider my="2" />
+            </FormContainer>
+          </View>
         </View>
-      </View>
-    </Modal>
-  );
+      </BottomDrawer>
+    );
+  } else {
+    return <View></View>;
+  }
 }
 
 const styles = StyleSheet.create({
   bottomNavigationView: {
     backgroundColor: '#fff',
     width: '100%',
-    height: 350,
+    height: 450,
     justifyContent: 'center',
     alignItems: 'center'
   },
