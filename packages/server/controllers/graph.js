fac77c7de1baebda3a88dd34ed465d4df84cb976
Yiğit Akbulut <yigitakbulut@icloud.com>
2022-03-06 16:12:34 +0100

    Remove forgotten console.logs

--- packages/server/controllers/graph.js
+++ packages/server/controllers/graph.js
@@ -183,17 +183,6 @@ module.exports.getProposalGraphs = async (req, res) => {
 
     res.status(200).send({ ...graph.toJSON(), forks });
   } catch (err) {
-    console.log(err);
-    console.log([
-      {
-        $match: {
-          'data.parentId': graphId,
-          user: { $ne: Types.ObjectId(`${req.user._id}`) }
-        },
-        ...LATEST_REVISION_AGGREGATOR,
-        ...USER_LOOKUP_PROJECTION
-      }
-    ]);
     res.status(404).send({ message: messages.GRAPH_NOT_FOUND, err });
   }
 };
