f8d4c0e1c02d22e610fff6678c41b42ac3f971a3
Alexander Milevski <alexander@linkurio.us>
2022-11-21 00:06:05 +0100

    Added Skia

--- packages/client/App.tsx
+++ packages/client/App.tsx
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider as UIProvider } from 'react-native-paper';
+import { Canvas, Circle, Group } from '@shopify/react-native-skia';
+
+const HelloWorld = () => {
+  const size = 256;
+  const r = size * 0.33;
+  return (
+    <Canvas style={{ flex: 1, width: '100%', height: 100 }}>
+      <Group blendMode="multiply">
+        <Circle cx={r} cy={r} r={r} color="cyan" />
+        <Circle cx={size - r} cy={r} r={r} color="magenta" />
+        <Circle cx={size / 2} cy={size - r} r={r} color="yellow" />
+      </Group>
+    </Canvas>
+  );
+};
 
 export default function App() {
   return (
     <UIProvider>
       <View style={styles.container}>
-        <Text>Don't open up Ap.tsx to start working on your app!</Text>
+        <HelloWorld />
         <StatusBar style="auto" />
       </View>
     </UIProvider>
