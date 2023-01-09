48493cd0edea6e42a35b94fb1534d9a6c1d4314d
Alexander Milevski <alexander@linkurio.us>
2023-01-09 15:10:42 +0100

    Switched to npm

--- packages/client/index.js
+++ packages/client/index.js
@@ -1,8 +1,18 @@
 import { registerRootComponent } from 'expo';
+import { Platform } from 'react-native';
+import { createRoot } from 'react-dom/client';
+import { createElement } from 'react';
 
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+if ('web' === Platform.OS) {
+  const rootTag = createRoot(
+    document.getElementById('root') ?? document.getElementById('main')
+  );
+  rootTag.render(createElement(App));
+} else {
+  // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+  // It also ensures that whether you load the app in Expo Go or in a native build,
+  // the environment is set up appropriately
+  registerRootComponent(App);
+}
